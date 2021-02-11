58f0dd70da66c1881aead353f60f49f9605d64b3
tangmack <macktang@gmail.com>
2021-02-10 19:06:45 -0700

    ordinary least squares working for highest point

--- least_squares.py
+++ least_squares.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 with open('highest_row_list.txt', 'r') as f:
     highest_row_strings = [line.rstrip('\n') for line in f]
@@ -35,12 +36,46 @@ print(img_sizes_strings)
 # convert from row column to x y
 y_highest = [abs(n-img_height) for n in highest_row_list]
 y_lowest = [abs(n-img_height) for n in lowest_row_list]
-x_center = [abs(n-img_width) for n in center_col_list]
+x_center = center_col_list
+# x_center = [abs(n-img_width) for n in center_col_list]
 print(y_highest)
 
+
+
+# form A
+x_regular = np.array(x_center).reshape( (len(x_center), 1))
+x_squared = np.square(np.array(x_center)).reshape( (len(x_center), 1) ) # reshape to avoid rank 1 array (26,) shape
+b_coeffs = np.ones(shape=x_squared.shape)
+
+b_highest = np.array(y_highest).reshape( (len(y_highest),1) )
+
+A = np.concatenate([x_squared,x_regular,b_coeffs],axis=1)
+
+# params_highest = (ATA)-1  * AT * b
+params_highest = np.linalg.pinv(   np.transpose(A).dot(A)   ).dot(  np.transpose(A).dot(y_highest)  )
+print("params_highest", params_highest)
+
+x_fit_high = np.linspace(0, np.max(x_regular), 1000)
+y_fit_high = params_highest[0] * x_fit_high**2 + params_highest[1] * x_fit_high + params_highest[2]
+
+
+
+
+
+print(b_highest.shape)
+
+print(x_squared.shape)
+print(b_coeffs.shape)
+
+A = np.concatenate([x_squared,b_coeffs],axis=1)
+print(A.shape)
+
+
+
+# plt.plot(x_squared,y_highest,'o')
 plt.plot(x_center, y_highest, '.')
 plt.plot(x_center, y_lowest, 'x')
-
+plt.plot(x_fit_high,y_fit_high, '.')
 
 plt.ylabel('y_highest')
 plt.show()
