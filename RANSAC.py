a32c0a0121af434b99fba75adfb01275ffc95a86
tangmack <macktang@gmail.com>
2021-02-14 16:17:37 -0700

    RANSAC working in loop

--- RANSAC.py
+++ RANSAC.py
@@ -32,7 +32,6 @@ def ordinary_least_squares(highest_row_list, lowest_row_list, center_col_list, i
     y_lowest = [abs(n-img_height) for n in lowest_row_list]
     x_center = center_col_list
     # x_center = [abs(n-img_width) for n in center_col_list]
-    print(y_highest)
 
 
 
@@ -45,7 +44,6 @@ def ordinary_least_squares(highest_row_list, lowest_row_list, center_col_list, i
     # b_lowest = np.array(y_lowest).reshape( (len(y_lowest),1) )
 
     A = np.concatenate([x_squared, x_regular, b_coeffs], axis=1)
-    print(A.shape)
 
     # params_highest = (ATA)-1  * AT * b
     params_highest = np.linalg.pinv(np.transpose(A).dot(A)).dot(np.transpose(A).dot(y_highest))
@@ -74,69 +72,59 @@ y_lowest = np.array([abs(n-img_height) for n in lowest_row_list])
 x_all = np.array(center_col_list)
 
 n = 3 # minimum number of data points required to estimate model parameters
-k = 1000 # number of iterations
+k = 10000 # number of iterations
 t = 10 # Threshold value to determine data points that are fit well by model
 
-iterations = 0
-maybeInliers = []
-bestModel = []
-bestErr = -1000
+best_error_high = 999999
+best_params_high = -1
 
-# while(iterations < k):
-random_indices_L = random.sample(range(0, len(center_col_list)), 3) # get 3 random not repeating indices
-x_random = [center_col_list[i] for i in random_indices_L]
-y_random_high = [highest_row_list[i] for i in random_indices_L]
+best_error_low = 999999
+best_params_low = -1
 
-y_random_low = [lowest_row_list[i] for i in random_indices_L]
+for iterations in range(0,k):
+    random_indices_L = random.sample(range(0, len(center_col_list)), 3) # get 3 random not repeating indices
+    x_random = [center_col_list[i] for i in random_indices_L]
+    y_random_high = [highest_row_list[i] for i in random_indices_L]
+    y_random_low = [lowest_row_list[i] for i in random_indices_L]
 
-x_center, y_highest_n, x_center, y_lowest_n, x_fit_high, y_fit_high, x_fit_low, y_fit_low, params_highest, params_lowest = ordinary_least_squares(y_random_high, y_random_low, x_random, img_sizes_list)
+    x_center, y_highest_n, x_center, y_lowest_n, x_fit_high, y_fit_high, x_fit_low, y_fit_low, params_highest, params_lowest = ordinary_least_squares(y_random_high, y_random_low, x_random, img_sizes_list)
+
+    # Count close points, within 10 units
+    y_fit_all_high = params_highest[0] * np.square(x_all) + params_highest[1] * x_all + params_highest[2]
+    y_fit_all_low = params_lowest[0] * np.square(x_all) + params_lowest[1] * x_all + params_lowest[2]
+
+    errors_high = np.absolute(y_highest - y_fit_all_high)
+    errors_low = np.absolute(y_lowest - y_fit_all_low)
+
+    number_good_high = np.count_nonzero(errors_high <= 10)
+    number_good_low = np.count_nonzero(errors_low <= 10)
+
+    # Sum error
+    total_error_high = np.sum(errors_high)
+    total_error_low = np.sum(errors_low)
+
+    if total_error_high < best_error_high:
+        best_error_high = total_error_high
+        best_params_high = params_highest
+
+    if total_error_low < best_error_low:
+        best_error_low = total_error_low
+        best_params_low = params_lowest
 
 
 
-
-# Count close points
-print(type(params_highest[0]))
-y_fit_all_high = params_highest[0] * np.square(x_all) + params_highest[1] * x_all + params_highest[2]
-y_fit_all_low = params_lowest[0] * np.square(x_all) + params_lowest[1] * x_all + params_lowest[2]
-
-errors_high = np.absolute(y_highest - y_fit_all_high)
-errors_low = np.absolute(y_lowest - y_fit_all_low)
-
-# errors_high = np.absolute(y_highest_n - y_fit_high)
-# errors_low = np.absolute(y_lowest_n - y_fit_low)
-
-print(errors_high.shape)
-
-number_good_high = np.count_nonzero(errors_high <= 10)
-number_good_low = np.count_nonzero(errors_low <= 10)
-print(number_good_high)
-print(number_good_low)
-
-# print("asdf")
-# print(np.array(y_highest).shape)
-# print(y_fit_high.shape)
-# errors_high = np.absolute(y_highest - y_fit_high)
-# print(errors_high.shape)
-
-# fit_y_high = np.array(map(lambda x: params_highest[0]*x**2 + params_highest[1]*x + params_highest[2], center_col_list))
-# print(fit_y_high)
-# print(type(fit_y_high))
-# print(type(np.array(y_highest)))
-# print(fit_y_high.shape)
-# print(np.array(y_highest).shape)
-# fit_y_high - np.array(y_highest)
-# errors_high = np.absolute(fit_y_high - np.array(y_highest))
-
-# sys.exit()
+x_linspace = np.linspace(0, np.max(x_all), 1000)
+y_fit_high = best_params_high[0] * x_linspace**2 + best_params_high[1] * x_linspace + best_params_high[2]
+y_fit_low = best_params_low[0] * x_linspace**2 + best_params_low[1] * x_linspace + best_params_low[2]
 
 
 # x_center, y_highest, x_center, y_lowest, x_fit_high, y_fit_high, x_fit_low, y_fit_low = ordinary_least_squares(highest_row_list, lowest_row_list, center_col_list, img_sizes_list)
 
-plt.plot(x_center, y_highest_n, 'o')
-plt.plot(x_center, y_lowest_n, 'o')
-plt.plot(x_fit_high, y_fit_high, '.')
+plt.plot(x_all, y_highest, 'o')
+plt.plot(x_all, y_lowest, 'o')
 
-plt.plot(x_fit_low, y_fit_low, 'x')
+plt.plot(x_linspace, y_fit_high, '.')
+plt.plot(x_linspace, y_fit_low, '.')
 
 # plt.plot(x_fit_high, errors_high,'x')
 
